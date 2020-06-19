5b8ae2478cc099489cc9f0593c6180344a7a2629
rikupin1105 <41769991+rikupin1105@users.noreply.github.com>
2020-06-20 01:17:18 +0900

    プロジェクト ファイルを追加します。

---
+++ Xamarin_Try_1/Xamarin_Try_1/MainViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Xamarin_Try_1
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public double Circumference { get; set; }
+        public double Area { get; set; }
+        public string Input { get; set; }
+        public ICommand Calc { get; }
+        public ICommand Reset { get; }
+
+        void CalcCircle()
+        {
+            if (Input != null)
+            {
+                var r = double.Parse(Input);
+                Circumference = 2 * 3.14 * r;
+                Area = 3.14 * r * r;
+
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Circumference)));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Area)));
+            }
+        }
+        void AllReset()
+        {
+            Circumference = 0;
+            Area = 0;
+            Input = "";
+
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Circumference)));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Area)));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Input)));
+        }
+        public MainViewModel()
+        {
+            Calc = new Command(CalcCircle);
+            Reset = new Command(AllReset);
+        }
+    }
+}
