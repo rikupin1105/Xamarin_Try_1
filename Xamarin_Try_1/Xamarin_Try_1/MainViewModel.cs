4034d9583f893b29fdbeb3fbf14e0abf885627c2
rikupin1105 <41769991+rikupin1105@users.noreply.github.com>
2020-06-20 22:03:34 +0900

    Update MainViewModel.cs

--- Xamarin_Try_1/Xamarin_Try_1/MainViewModel.cs
+++ Xamarin_Try_1/Xamarin_Try_1/MainViewModel.cs
@@ -9,36 +9,18 @@ namespace Xamarin_Try_1
         public event PropertyChangedEventHandler PropertyChanged;
         public double Circumference { get; set; }
         public double Area { get; set; }
-        public string Input { get; set; }
-        public ICommand Calc { get; }
-        public ICommand Reset { get; }
-
-        void CalcCircle()
+        public double Input 
         {
-            if (Input != null)
+            set
             {
-                var r = double.Parse(Input);
-                Circumference = 2 * 3.14 * r;
-                Area = 3.14 * r * r;
+                    var r = value;
+                    Circumference = 2 * 3.14 * r;
+                    Area = 3.14 * r * r;
 
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Circumference)));
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Area)));
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Circumference)));
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Area)));
+                
             }
-        }
-        void AllReset()
-        {
-            Circumference = 0;
-            Area = 0;
-            Input = "";
-
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Circumference)));
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Area)));
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Input)));
-        }
-        public MainViewModel()
-        {
-            Calc = new Command(CalcCircle);
-            Reset = new Command(AllReset);
         }
     }
 }
